d646468bc90239128256ac81abb90f96b8aa4453
Zulfikar Hilman P <52442172+hyylman@users.noreply.github.com>
2025-01-25 19:58:50 +0700

    Update Jb1Controller.php

--- app/Http/Controllers/Jb1Controller.php
+++ app/Http/Controllers/Jb1Controller.php
@@ -1,16 +1,57 @@
+// <?php
+
+// namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+// use App\Models\jb1;
+
+// class Jb1Controller extends Controller
+// {
+//     public function index()
+//     {
+//         $title = 'Jumlah Penduduk Jabar';
+//         $jb1 = jb1::all();
+//         return view('tematik.index', compact('jb1', 'title'));
+//     }
+// }
+
+// update:
+
 <?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\jb1;
+use App\Models\jb2;
+use App\Models\jb3;
+
 
 class Jb1Controller extends Controller
 {
-    public function index()
+    public function jb1()
     {
-        $title = 'Jumlah Penduduk Jabar';
         $jb1 = jb1::all();
-        return view('tematik.index', compact('jb1', 'title'));
+        return view('jb1', compact('jb1'));
+    }
+
+    public function jb2()
+    {
+        $jb2 = jb1::all();
+        return view('jb2', compact('jb2'));
+    }
+
+    public function jb3()
+    {
+        $jb3 = jb1::all();
+        return view('jb3', compact('jb3'));
+    }
+
+    public function jb4()
+    {
+        $jb4 = jb1::all();
+        return view('jb4', compact('jb4'));
     }
 }
+
+
