85eb439a8a19ceb12149c5a6a58ebe5deb1a42bc
Zulfikar Hilman P <52442172+hyylman@users.noreply.github.com>
2025-01-25 11:18:18 +0700

    upload jb1

---
+++ database/migrations/2025_01_24_074137_create_jb1s_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jb1s', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);  
+            $table->string('alt_name')->default('');  
+            $table->double('latitude')->default(0)->nullable(false);  
+            $table->double('longitude')->default(0)->nullable(false);  
+            $table->bigInteger('population')->default(0)->nullable(false);
+            $table->bigInteger('perokok')->default(0)->nullable(false);
+            $table->enum('type_polygon', ['Polygon', 'MultiPolygon'])->default('Polygon');  
+            $table->longText('polygon')->nullable(true);  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jb1s');
+    }
+};
